9b3f0e8a97ae776e99829a7efedc272dd98f27f5
swati mandal <158429232+swati-aracturus@users.noreply.github.com>
2024-06-21 17:49:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./components/Home";
+import Register from "./components/Register";
+import Google from "./components/Google";
+import Verify from "./components/Verify";
+import Change from "./components/Change";
+import Emailver from "./components/Emailverified";
+import Welcome from "./components/Welcome";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Signup />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/google" element={<Google />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/change" element={<Change />} />
+          <Route path="/email-verified" element={<Emailver />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
