9aa54aec71ca5fc1215975b195e59e9de5347239
swati mandal <158429232+swati-aracturus@users.noreply.github.com>
2024-06-24 11:00:36 +0530

    .

--- src/components/header.js
+++ src/components/header.js
@@ -11,18 +11,56 @@ export default function Header() {
     <>
       {" "}
       <div className="bg-pink-400  ">
-        <Slidenav/>
+        <Slidenav />
         <div className="flex justify-center  align-center items-center px-[15%]  lx:px-[5%]">
           <div className="pt-[38%] ">
             <p className="text-center text-white">WE ARE #1 ON THE MARKET</p>
-            <h1 className="text-7xl text-white font-semibold  sd:text-4xl text-center py-4 pm:text-2xl ">We're Here To Help You Navigate While Traveling</h1>
-            <p className="text-center text-white text-lg font-medium py-6">You'll get comprehensive results based on the provided location.</p>
-<div className="flex flex-wrap justify-center h-24 rounded-[40px] align-center items-center px-[15%] my-8 bg-white">
-<div>fudfheji</div>
-<div>fudfheji</div>
+            <h1 className="text-7xl text-white font-semibold  sd:text-4xl text-center py-4 pm:text-2xl ">
+              We're Here To Help You Navigate While Traveling
+            </h1>
+            <p className="text-center text-white text-lg font-medium py-6">
+              You'll get comprehensive results based on the provided location.
+            </p>
+            <div className="flex flex-wrap justify-center h-24 rounded-[40px] align-center items-center px-[5%] my-8 bg-white">
+             
+<form class="w-2/5  px-2 mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 border-bottom rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+    </div>
+</form>
 
-<div>fudfheji</div></div>
+<form class="w-2/5 px-2 mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+          </div>
+</form>
 
+<form class="w-1/5 px-2 mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border-bottom-2 border-gray-300   rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+            </div>
+</form>
+
+             </div>
+            
           </div>
         </div>
       </div>
